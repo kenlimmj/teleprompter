5643e06b64ad30a14723089e61d64be35519f55b
Lim Mingjie Kenneth <kenlimmj@gmail.com>
2013-11-01 18:23:47 -0400

    Fixed the scroll behavior

--- js/mouse.js
+++ js/mouse.js
@@ -1,42 +1,23 @@
-function mouseSpeed(event) {
-    var rolled = 0; // sensitivity adjustment
+function mouseSpeed(e) {
+    // Sensitivity tweak
+    var scrollAdjust = 3;
 
-    if('wheelDelta' in event) {
-        rolled = event.wheelDelta / 30; // all except firefox
-    } else {
-        //firefox
-        rolled = 40 * event.detail / 30;
-    }
+    // Initialize a cross-browser friendly window event handle
+    var e = window.event || e;
 
-    popupWindow.scrollBy(0,rolled);
+    // Detect the change in the scroll wheel. Return -1 on scroll upwards, and +1 on scroll downwards
+    var delta = Math.min(1, Math.max(-1, (e.wheelDelta || -e.detail)));
 
-    // document.getElementById("rangeInput").value -= rolled;
-    // updateTextInput(document.getElementById("rangeInput").value);
+    console.log(delta);
+
+    // Scroll the popup in the correct direction, factoring in sensitivity adjustment
+    popupWindow.scrollBy(0,-scrollAdjust*delta);
 }
 
 function initMouse() {
-    // IE Opera Chrome Safari
-    document.addEventListener("mousewheel", mouseSpeed, false);
-    // Firefox
-    document.addEventListener("DOMMouseSpeed", mouseSpeed, false);
-}
+    // IE9, Opera, Chrome, Safari
+    document.addEventListener("mousewheel",mouseSpeed,false);
 
-function updateTextInput(val) {
-    switch(val) {
-        case '0':
-            document.getElementById('rangeDisplay').value="Fucking Fast";
-            break;
-        case '10':
-            document.getElementById('rangeDisplay').value="Fast";
-            break;
-        case '20':
-            document.getElementById('rangeDisplay').value="Standard";
-            break;
-        case '30':
-            document.getElementById('rangeDisplay').value="Slow";
-            break;
-        case '40':
-            document.getElementById('rangeDisplay').value="Fucking Slow";
-            break;
-    }
+    // Firefox
+    document.addEventListener("DOMMouseSpeed",mouseSpeed,false);
 }

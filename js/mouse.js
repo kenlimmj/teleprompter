134f9f10a1bd93b34b541625eb3ba2c59a1bc5dc
Lim Mingjie Kenneth <kenlimmj@gmail.com>
2013-11-04 22:27:41 -0500

    Commented up the mouse.js file and added a catch for the popup window

--- js/mouse.js
+++ js/mouse.js
@@ -1,16 +1,22 @@
 // An instance detects the direction of mouse-wheel scroll anywhere and scrolls the popupWindow accordingly
+// Precondition: The input argument "e" must be a valid browser event
 function mouseSpeed(e) {
     // Sensitivity tweak
+    // A larger value means the same mousewheel displacement scrolls more
     var scrollAdjust = 3;
 
     // Initialize a cross-browser friendly window event handle
     var e = window.event || e;
 
     // Detect the change in the scroll wheel. Return -1 on scroll upwards, and +1 on scroll downwards
+    // The actual value of wheelDelta/detail is much larger (in the hundreds), but by doing this we debounce the event listener so
+    // it won't be too sensitive
     var delta = Math.min(1, Math.max(-1, (e.wheelDelta || -e.detail)));
 
     // Scroll the popup in the correct direction, factoring in sensitivity adjustment
-    popupWindow.scrollBy(0,-scrollAdjust*delta);
+    if (popupWindow && !popupWindow.closed) {
+        popupWindow.scrollBy(0,-scrollAdjust*delta);
+    }
 }
 
 // An instance adds a cross-browser event listener to monitor mouse-wheel scrolls

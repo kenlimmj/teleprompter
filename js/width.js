a2207e8fccabc98734662f55ffb240ed1ac0628a
Lim Mingjie Kenneth <kenlimmj@gmail.com>
2013-11-04 22:27:10 -0500

    Added a controller for updating and managing the width adjustments

---
+++ js/width.js
@@ -0,0 +1,30 @@
+function updateWidthInput(val) {
+  switch(val) {
+      case '100':
+          document.getElementById('widthDisplay').value="American";
+          break;
+      case '90':
+          document.getElementById('widthDisplay').value="Really Fat";
+          break;
+      case '80':
+          document.getElementById('widthDisplay').value="Fat";
+          break;
+      case '70':
+          document.getElementById('widthDisplay').value="Standard";
+          break;
+      case '60':
+          document.getElementById('widthDisplay').value="Thin";
+          break;
+      case '50':
+          document.getElementById('widthDisplay').value="Anorexic";
+          break;
+      default:
+          document.getElementById('widthDisplay').value=val;
+          break;
+  }
+
+  if (popupWindow && !popupWindow.closed) {
+    popupWindow.document.getElementById('screen').style.width = val+"vw";
+  }
+  document.getElementById('prompt').style.width = val+"%";
+}
